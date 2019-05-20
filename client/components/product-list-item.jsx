28423e3a49cb613ef0561f1f73d55b7de9c9864b
Jonathan <kakashi169@gmail.com>
2019-05-20 20:31:34 +0000

    added css styling and general bootstrap tweaks

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -4,23 +4,23 @@ export default class ProductListItem extends React.Component {
   render() {
     const product = this.props.product;
     return (
-      <div className="col-md-4">
-        <div className="imgContainer col">
+      <div className="card-deck mt-3 col-md-4">
+        <div className="card bg-light">
           <img
             src={product.image}
             alt="product picture"
-            className="img-thumbnail"
+            className="img-thumbnail product-image"
           />
-        </div>
-        <div className="productDescriptionContainer">
-          <div className="productName col">
-            <h3 className="font-weight-bold mb-0">{product.name}</h3>
-          </div>
-          <div className="productPrice col">
-            <div className="font-weight-light">{'$' + (product.price / 100).toFixed(2)}</div>
-          </div>
-          <div className="productDescription col">
-            <div>{product.shortDescription}</div>
+          <div>
+            <div className="productName col">
+              <h5 className="font-weight-bold mb-0">{product.name}</h5>
+            </div>
+            <div className="productPrice ml-2 col">
+              <div className="font-weight-light">{'$' + (product.price / 100).toFixed(2)}</div>
+            </div>
+            <div className="productDescription col">
+              <div>{product.shortDescription}</div>
+            </div>
           </div>
         </div>
       </div>
