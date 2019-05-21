4c07f78aff6d691b4c286668584a886b4f264524
Jonathan <kakashi169@gmail.com>
2019-05-21 23:17:06 +0000

    added errors into code for 500 error

--- server/public/api/functions.php
+++ server/public/api/functions.php
@@ -4,9 +4,9 @@
 function error_handler($error) {
     $output = [
         'success' => false,
-        'error' => $error -> getMessage()
+        'error' => $error -> getMessage(),
     ];
-
+    http_response_code(500);
     $json_output = json_encode($output);
 
     echo $json_output;
--- server/public/api/products.php
+++ server/public/api/products.php
@@ -3,8 +3,9 @@
 require_once('functions.php');
 
 set_exception_handler('error_handler');
+doStuff();
 
-$output = file_get_contents('./dummy-products-list.json');
+$output = file_get_contents('dummy-products-list.json');
 
 echo $output;
 
