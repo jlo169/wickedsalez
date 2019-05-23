06c7311038af3a188e5e461a9423b04a3ca91688
Jonathan <kakashi169@gmail.com>
2019-05-23 04:23:32 +0000

    hit half point in be5

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -4,14 +4,20 @@ require_once('functions.php');
 require_once('db_connection.php');
 
 set_exception_handler('error_handler');
-
 startup();
 
 if(!$conn) {
   throw new Exception( mysqli_connect_error() );
 }
 
-$query = "SELECT `id`, `name`, `price`, `image`, `shortDescription` FROM `products`";
+$whereClause = '';
+
+if (!empty($_GET["id"])) {
+  $id = $_GET["id"];
+  $whereClause = "WHERE `id` = $id";
+}
+
+$query = "SELECT `id`, `name`, `price`, `image`, `shortDescription` FROM `products` $whereClause";
 $result = mysqli_query($conn, $query);
 
 if(!$result) {
